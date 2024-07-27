8c006c9494729d5f1ce2cadb1c089e1073e6e0e8
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-27 18:33:50 +0630

    feat(onboarding): onboarding ui added

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -1,7 +1,29 @@
-const OnboardingPage = () => {
+import AccountProfile from "@/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs/server";
+
+const OnboardingPage = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = {} as any;
+
+  const userData = {
+    id: user.id,
+    objectId: userInfo?._id,
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user.imageUrl,
+  };
   return (
-    <main>
+    <main className="flex flex-col justify-start max-w-3xl px-10 py-20 mx-auto">
       <h1 className="head-text">Onboarding</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete your profile now, to use Threds.
+      </p>
+      <section className="p-10 mt-9 bg-dark-2">
+        <AccountProfile user={userData} btnTitle="Continue" />
+      </section>
     </main>
   );
 };
