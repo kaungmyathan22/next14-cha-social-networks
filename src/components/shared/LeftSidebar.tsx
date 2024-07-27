f2aafba8d602703742375cd2fc138a13d614246c
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-27 14:07:02 +0630

    feat(component): top / left / right component added

---
+++ src/components/shared/LeftSidebar.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
+import { sidebarLinks } from "@/constants";
+
+const LeftSidebar = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const { userId } = useAuth();
+  if (!userId) return null;
+
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex flex-col flex-1 w-full gap-6 px-6">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          if (link.route === "/profile") link.route = `${link.route}/${userId}`;
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`leftsidebar_link ${isActive && "bg-primary-500 "}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+
+      <div className="px-6 mt-10">
+        <SignedIn>
+          <SignOutButton redirectUrl={"/sign-in"}>
+            <div className="flex gap-4 p-4 cursor-pointer">
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+
+              <p className="text-light-2 max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
+      </div>
+    </section>
+  );
+};
+
+export default LeftSidebar;
