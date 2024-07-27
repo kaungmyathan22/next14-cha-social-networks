42d997627440e5ceea5700c43b8b85cb5af4b5ab
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-27 12:45:37 +0630

    update(): chagne layout to be dark theme

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import { dark } from "@clerk/themes";
 export const metadata = {
   title: "Threads",
   description: "A Next.js Meta Threads Application",
@@ -10,7 +11,11 @@ const inter = Inter({ subsets: ["latin"] });
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en">
         <body className={`${inter.className} bg-dark-1`}>{children}</body>
       </html>
--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,4 +1,7 @@
+import Bottombar from "@/components/shared/Bottombar";
+import Topbar from "@/components/shared/Topbar";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
@@ -16,9 +19,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+          <main className="flex flex-row">
+            {/* <LeftSidebar /> */}
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            {/* <RightSidebar /> */}
+            rightsidebar
+          </main>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   );
