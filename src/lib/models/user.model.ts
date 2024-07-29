efc715270c5fc5d3868a8f9ec89bb1fb571606f6
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-29 21:41:33 +0630

    update(user): user model updates with thread / community relations

--- src/lib/models/user.model.ts
+++ src/lib/models/user.model.ts
@@ -16,10 +16,22 @@ const userSchema = new mongoose.Schema({
   },
   image: String,
   bio: String,
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
   onboarded: {
     type: Boolean,
     default: false,
   },
+  communities: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Community",
+    },
+  ],
 });
 
 const User = mongoose?.models?.User || mongoose.model("User", userSchema);
