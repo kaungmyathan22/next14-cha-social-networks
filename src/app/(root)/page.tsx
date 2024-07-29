f48dc948b17ef56a616045bbcb70af9147b0c3de
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-29 21:27:32 +0630

    feat(): user must be onboarded before using the app

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,12 @@
-import { UserButton } from "@clerk/nextjs";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <main>
-      amigos
-      <UserButton signInUrl="/sign-in" />
-    </main>
-  );
+export default async function Home() {
+  const user = await currentUser();
+  if (!user) return redirect("/sign-in");
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+  return <main className="text-white">Home</main>;
 }
