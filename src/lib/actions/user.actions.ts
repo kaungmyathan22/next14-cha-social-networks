c4000c58c0fd588d4d9226646f0588f32f585090
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-29 15:37:58 +0630

    feat(actions): fetch user action added

---
+++ src/lib/actions/user.actions.ts
@@ -0,0 +1,12 @@
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose";
+
+export async function fetchUser(userId: string) {
+  try {
+    connectToDB();
+
+    return await User.findOne({ id: userId });
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user: ${error.message}`);
+  }
+}
