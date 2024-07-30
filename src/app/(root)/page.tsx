71d61acd3c7bbac704d867484c973ea94de79a6b
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-07-30 15:27:19 +0630

    update(): return null instead of signin

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -10,7 +10,7 @@ export default async function Home({
   searchParams: { [key: string]: string | undefined };
 }) {
   const user = await currentUser();
-  if (!user) return redirect("/sign-in");
+  if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
